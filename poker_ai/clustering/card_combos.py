fd507693b23a75177bb2339c392b6e50fc0c95b3
hollybed <32048612+hollybed@users.noreply.github.com>
2023-10-28 17:28:43 +0800

    Update card_combos.py

--- poker_ai/clustering/card_combos.py
+++ poker_ai/clustering/card_combos.py
@@ -1,7 +1,9 @@
+import concurrent.futures
 import logging
 from typing import List
 from itertools import combinations
 import operator
+import multiprocessing
 
 import numpy as np
 from tqdm import tqdm
@@ -9,103 +11,41 @@ from tqdm import tqdm
 from poker_ai.poker.card import Card
 from poker_ai.poker.deck import get_all_suits
 
-
 log = logging.getLogger("poker_ai.clustering.runner")
 
+def get_card_combos(_cards, num_cards: int) -> np.ndarray:
+    return np.array([c for c in combinations(_cards, num_cards)])
+
+def create_info_combos_task(start_combos: np.ndarray, publics: np.ndarray, _cards) -> np.ndarray:
+    our_cards: List[Card] = []
+    for combos in start_combos:
+        sorted_combos: List[Card] = sorted(list(combos), key=operator.attrgetter("eval_card"), reverse=True,)
+        for public_combo in publics:
+            sorted_public_combo: List[Card] = sorted(list(public_combo), key=operator.attrgetter("eval_card"), reverse=True,)
+            if not np.any(np.isin(sorted_combos, sorted_public_combo)):
+                hand: np.array = np.array(sorted_combos + sorted_public_combo)
+                our_cards.append(hand)
+    return np.array(our_cards)
 
 class CardCombos:
-    """This class stores combinations of cards (histories) per street."""
-
-    def __init__(
-        self, low_card_rank: int, high_card_rank: int,
-    ):
+    def __init__(self, low_card_rank: int, high_card_rank: int):
         super().__init__()
-        # Sort for caching.
         suits: List[str] = sorted(list(get_all_suits()))
         ranks: List[int] = sorted(list(range(low_card_rank, high_card_rank + 1)))
-        self._cards = np.array(
-            [Card(rank, suit) for suit in suits for rank in ranks]
-        )
-        self.starting_hands = self.get_card_combos(2)
-        self.flop = self.create_info_combos(
-            self.starting_hands, self.get_card_combos(3)
-        )
+        self._cards = np.array([Card(rank, suit) for suit in suits for rank in ranks])
+        self.starting_hands = get_card_combos(self._cards, 2)
+        self.flop = self.create_info_combos(self.starting_hands, get_card_combos(self._cards, 3))
         log.info("created flop")
-        self.turn = self.create_info_combos(
-            self.starting_hands, self.get_card_combos(4)
-        )
+        self.turn = self.create_info_combos(self.starting_hands, get_card_combos(self._cards, 4))
         log.info("created turn")
-        self.river = self.create_info_combos(
-            self.starting_hands, self.get_card_combos(5)
-        )
+        self.river = self.create_info_combos(self.starting_hands, get_card_combos(self._cards, 5))
         log.info("created river")
 
-    def get_card_combos(self, num_cards: int) -> np.ndarray:
-        """
-        Get the card combinations for a given street.
-
-        Parameters
-        ----------
-        num_cards : int
-            Number of cards you want returned
-
-        Returns
-        -------
-            Combos of cards (Card) -> np.ndarray
-        """
-        return np.array([c for c in combinations(self._cards, num_cards)])
-
-    def create_info_combos(
-        self, start_combos: np.ndarray, publics: np.ndarray
-    ) -> np.ndarray:
-        """Combinations of private info(hole cards) and public info (board).
-
-        Uses the logic that a AsKsJs on flop with a 10s on turn is the same
-        as AsKs10s on flop and Js on turn. That logic is used within the
-        literature as well as the logic where those two are different.
-
-        Parameters
-        ----------
-        start_combos : np.ndarray
-            Starting combination of cards (beginning with hole cards)
-        publics : np.ndarray
-            Public cards being added
-        Returns
-        -------
-            Combinations of private information (hole cards) and public
-            information (board)
-        """
-        if publics.shape[1] == 3:
-            betting_stage = "flop"
-        elif publics.shape[1] == 4:
-            betting_stage = "turn"
-        elif publics.shape[1] == 5:
-            betting_stage = "river"
-        else:
-            betting_stage = "unknown"
-        our_cards: List[Card] = []
-        for combos in tqdm(
-            start_combos,
-            dynamic_ncols=True,
-            desc=f"Creating {betting_stage} info combos",
-        ):
-            # Descending sort combos.
-            sorted_combos: List[Card] = sorted(
-                list(combos),
-                key=operator.attrgetter("eval_card"),
-                reverse=True,
-            )
-            for public_combo in publics:
-                # Descending sort public_combo.
-                sorted_public_combo: List[Card] = sorted(
-                    list(public_combo),
-                    key=operator.attrgetter("eval_card"),
-                    reverse=True,
-                )
-                if not np.any(np.isin(sorted_combos, sorted_public_combo)):
-                    # Combine hand and public cards.
-                    hand: np.array = np.array(
-                        sorted_combos + sorted_public_combo
-                    )
-                    our_cards.append(hand)
-        return np.array(our_cards)
+    def create_info_combos(self, start_combos: np.ndarray, publics: np.ndarray) -> np.ndarray:
+        num_processes = multiprocessing.cpu_count()
+        chunk_size = len(start_combos) // num_processes
+        chunks = [start_combos[i * chunk_size:(i + 1) * chunk_size] for i in range(num_processes)]
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = list(executor.map(create_info_combos_task, chunks, [publics]*num_processes, [self._cards]*num_processes))
+        our_cards = np.concatenate(results)
+        return our_cards
